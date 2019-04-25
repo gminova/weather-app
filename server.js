c203cb3c148b4e7208b9c1b7a81d06d1836f0eb0
Emil Vaklinov <emil_vaklinov@yahoo.com>
2019-04-25 02:01:11 +0100

    api working

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const request = require('request');
 const app = express()
 
-const apiKey = '*****************';
+const apiKey = 'e6884c3b42dc67cf16d20cf6a2ded3ca';
 
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
