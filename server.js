78867a0d2419c95380622554d5f9e77617dab977
gminova <minova1995@gmail.com>
2019-04-26 12:18:44 +0100

    heroku testing

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const request = require('request');
 const app = express()
+const port = process.env.PORT || 3000;
 
 
 const apiKey = 'e6884c3b42dc67cf16d20cf6a2ded3ca';
@@ -34,6 +35,6 @@ app.post('/', function (req, res) {
   });
 })
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+app.listen(port, function () {
+  console.log('Example app listening on port!')
 })
